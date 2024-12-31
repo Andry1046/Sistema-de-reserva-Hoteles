5b05ebcea27a2453193bef89944e81371defe940
Andry <andry46@outlook.es>
2024-12-30 23:00:19 -0600

    Clase Reserva

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace Sistemas_de_reservas_Hoteles
 {
@@ -14,5 +15,53 @@ namespace Sistemas_de_reservas_Hoteles
 
 
         }
+    }
+
+    class Habitacion
+    {
+
+        private int Numero;
+        private string Tipo;
+        private string Estado;
+        
+        public Habitacion(int Numero, string Tipo, string Estado)
+        {
+            this.Numero = Numero;
+            this.Tipo = Tipo;
+            this.Estado = Estado;
+
+        }
+
+        public void ActualizarEstado(string nuevoestado) => Estado = nuevoestado;
+
+        public void MostrarInfo() => Console.WriteLine($"Habitacion #{Numero} Clase/Tipo: {Tipo} Estado: {Estado}");
+
+    }
+
+
+    class Reserva
+    {
+
+        private string NombreCliente;
+        private string Habitacion;
+        private string Fecha;
+
+
+        public Reserva(string NombreCliente, string Habitacion, string Fecha)
+        {
+            this.NombreCliente = NombreCliente;
+            this.Habitacion = Habitacion;
+            this.Fecha = Fecha;
+            
+        }
+
+        //Asocia una habitacion al cliente
+        public void RegistrarReserva(Habitacion hab)
+        {
+
+        }
+
+        //LIbera la habitacion reservada
+
     }
 }
